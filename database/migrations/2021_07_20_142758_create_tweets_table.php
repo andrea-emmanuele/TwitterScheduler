1ddb7bebbda9b6fca6ee5de20d895939f537f0fb
andrea-emmanuele <contact@andreaemmanuele.it>
2021-07-20 16:51:49 +0200

    Created Tweets Migration and relative model, controller and factory

---
+++ database/migrations/2021_07_20_142758_create_tweets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->text('message');
+            $table->string('media_path')->nullable();
+            $table->dateTime('published_at');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('hashtag_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
